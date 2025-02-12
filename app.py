8ab3a784e438549db513dbb19093fa080edc0098
Arjun-sathiesh <115931532+Arjun-sathiesh@users.noreply.github.com>
2025-02-12 23:31:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import time
-import PdfReader
+from pypdf import PdfReader
 import google.generativeai as genai
 
 # Dummy user credentials (Replace with a database in a real system)
